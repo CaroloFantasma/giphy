ff97664bb674130b720003334514b3c66d9feaef
Carolina Lastra <carolastra.cofre@gmail.com>
2018-06-18 23:59:00 -0400

    avances

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+window.onload = (() => {
+
+  const searchButton = document.getElementById('searchBtn');
+
+  searchButton.addEventListener('click', function (event) {
+    const searchingGif = document.getElementById('gifSearching').value;
+    // console.log(searchingGif);
+
+    // return result (searchingGif)
+  
+  });
+});
